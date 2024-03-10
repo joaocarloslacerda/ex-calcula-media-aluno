e43b0de73d2b73c6269f8f33e86464e89b6bf40c
joao <juniorlacerdasilva@gmail.com>
2024-03-10 16:06:02 -0300

    adicionado cast para Number nos valores de nota para corrigir o problema na validação do cálculo

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function calculaMedia(){
 
     let divResultado = document.getElementById("resultado");
 
-    const notaUm = document.getElementById("notaUm").value;
-    const notaDois = document.getElementById("notaDois").value;
-    const notaTres = document.getElementById("notaTres").value;
+    const notaUm = Number(document.getElementById("notaUm").value);
+    const notaDois = Number(document.getElementById("notaDois").value);
+    const notaTres = Number(document.getElementById("notaTres").value);
 
     let resultado = (notaUm+notaDois+notaTres) / 3;
 
@@ -18,4 +18,6 @@ function calculaMedia(){
         divResultado.style.marginLeft = "105px";
         divResultado.style.color = "red";
     }
+
+    console.log(resultado)
 }
